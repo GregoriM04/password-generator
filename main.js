52c2fa2b2c2c49d2c3f3916e61b5227336336813
Gregori Martinez <gregorim143@gmail.com>
2024-05-07 21:45:42 -0400

    Dark mode functionality added

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("Hello!");
+const body = document.querySelector("body");
+const modeToggle = body.querySelector(".slider");
+
+// Dark mode settings
+let getMode = localStorage.getItem("mode");
+if (getMode && getMode === "dark") {
+  body.classList.toggle("dark");
+}
+
+modeToggle.addEventListener("click", () => {
+  body.classList.toggle("dark");
+  if (body.classList.contains("dark")) {
+    localStorage.setItem("mode", "dark");
+  } else {
+    localStorage.setItem("mode", "light");
+  }
+});
