a2eb974981eb7a8d7b0158b302afe6d9a06d1698
Gregori Martinez <gregorim143@gmail.com>
2024-06-09 00:20:04 -0400

    input checker added

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const body = document.querySelector("body");
 const modeToggle = body.querySelector(".slider");
 
-// Dark mode settings
+/* Dark mode settings */
 let getMode = localStorage.getItem("mode");
 if (getMode && getMode === "dark") {
   body.classList.toggle("dark");
@@ -16,7 +16,7 @@ modeToggle.addEventListener("click", () => {
   }
 });
 
-// Main functionality
+/* Main functionality */
 const selectOption = document.querySelector("#length");
 const generateButton = document.querySelector("#generate");
 const copyButton = document.querySelector("#copy-btn");
@@ -30,65 +30,104 @@ const includeUppercase = document.querySelector("#uppercase");
 const includeNumbers = document.querySelector("#numbers");
 const includeSymbols = document.querySelector("#symbols");
 
+// characters to be used in new passwords
 let lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
 let upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let numbers = "0123456789";
 let symbols = "!@#$%ˆ&*()-_=+[]{}|;:,.<>?/";
 
-generateButton.addEventListener("click", () => {
-  let collection = selectOption.selectedOptions;
-  let newGeneration = "";
+// input checker
+function checkboxChecker() {
+  let counter = 0;
   let additions = "";
-  let value = "";
-
-  for (i = 0; i < collection.length; i++) {
-    value += collection[i].label;
-  }
-
   if (includeLowercase.checked) {
     additions += lowerCaseLetters;
+    counter += 1;
   }
   if (includeUppercase.checked) {
     additions += upperCaseLetters;
+    counter += 1;
   }
   if (includeNumbers.checked) {
     additions += numbers;
+    counter += 1;
   }
   if (includeSymbols.checked) {
     additions += symbols;
+    counter += 1;
   }
 
-  for (j = 0; j < value; j++) {
-    newGeneration += additions.charAt(
-      Math.floor(Math.random() * additions.length)
-    );
-  }
-
-  if (value >= 10) {
-    passStrength.innerHTML = "This is a <u>STRONG</u> password!";
-    passStrength.style.color = "#4EA72E";
-    passwords.style.borderColor = "#4EA72E";
-  } else if (value >= 6) {
-    passStrength.innerHTML = "This is an <u>Acceptable</u> password!";
-    passStrength.style.color = "#FFC000";
-    passwords.style.borderColor = "#FFC000";
+  if (counter == 0) {
+    return "none";
   } else {
-    passStrength.innerHTML = "This is a <u>Weak</u> password!";
-    passStrength.style.color = "#FF0000";
-    passwords.style.borderColor = "#FF0000";
+    return additions;
   }
+};
 
-  passwords.value = newGeneration;
+// click to generate a new password
+generateButton.addEventListener("click", () => {
+  if (checkboxChecker() == "none") {
+    passwords.style.borderColor = "#ff0000";
+    passStrength.innerHTML = "Please, select an option!";
+    passStrength.style.color = "#ff0000";
+    setTimeout(() => {
+      passwords.style.borderColor = "#afa8ba";
+      passStrength.innerHTML = "";
+      passwords.value = "";
+    }, 2000);
+  } else {
+    let collection = selectOption.selectedOptions;
+    let newGeneration = "";
+    let inputsAdded = checkboxChecker();
+    let value = "";
+
+    for (i = 0; i < collection.length; i++) {
+      value += collection[i].label;
+    }
+
+    for (j = 0; j < value; j++) {
+      newGeneration += inputsAdded.charAt(
+        Math.floor(Math.random() * inputsAdded.length)
+      );
+    }
+
+    if (value >= 10) {
+      passStrength.innerHTML = "This is a <u>STRONG</u> password!";
+      passStrength.style.color = "#4EA72E";
+      passwords.style.borderColor = "#4EA72E";
+    } else if (value >= 6) {
+      passStrength.innerHTML = "This is an <u>Acceptable</u> password!";
+      passStrength.style.color = "#FFC000";
+      passwords.style.borderColor = "#FFC000";
+    } else {
+      passStrength.innerHTML = "This is a <u>Weak</u> password!";
+      passStrength.style.color = "#FF0000";
+      passwords.style.borderColor = "#FF0000";
+    }
+
+    passwords.value = newGeneration;
+  }
 });
 
+// click to copy the new password
 copyButton.addEventListener("click", () => {
-  navigator.clipboard.writeText(passwords.value);
-  copySpantext.innerHTML = "Copied!";
-  copyButton.style.backgroundColor = "#4EA72E";
-  copyIcon.setAttribute("name", "checkbox");
-  setTimeout(() => {
-    copySpantext.innerHTML = "Copy";
-    copyButton.style.backgroundColor = "#482ad8";
-    copyIcon.setAttribute("name", "copy");
-  }, 2000);
+  if (passwords.value == "") { // check if a password was generated
+    passwords.style.borderColor = "#ff0000";
+    passStrength.innerHTML = "Must generate first! :)";
+    passStrength.style.color = "#ff0000";
+    setTimeout(() => {
+      passwords.style.borderColor = "#afa8ba";
+      passStrength.innerHTML = "";
+    }, 2000);
+  } else {
+    navigator.clipboard.writeText(passwords.value);
+    copySpantext.innerHTML = "Copied!";
+    copyButton.style.backgroundColor = "#4EA72E";
+    copyIcon.setAttribute("name", "checkbox");
+    setTimeout(() => {
+      copySpantext.innerHTML = "Copy";
+      copyButton.style.backgroundColor = "#482ad8";
+      copyIcon.setAttribute("name", "copy");
+    }, 2000);
+  }
 });
